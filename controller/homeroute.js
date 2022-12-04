7090eafb87373b41cc45d0d7696e53ef92540092
Anthony Speece <anthonyspeece@gmail.com>
2022-12-04 00:12:54 -0600

    getting the login in page routes and models setup

---
+++ controller/homeroute.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const { User } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
